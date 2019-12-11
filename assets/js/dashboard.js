0b4038d51f7efa0c56843d6666f964e0f9b7ff64
a-vargasmarte <a.vargasmarte@gmail.com>
2019-12-11 15:02:04 -0500

    merged 2017 school data, integrated it in mapbox, and added click handler on map to get school properties

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -6,9 +6,23 @@ mapboxgl.accessToken = mapboxToken;
 const map = new mapboxgl.Map({
   container: "map",
   style: "mapbox://styles/a-vargasmarte/ck3xpzp1m2gxt1dmyttn4qiv5",
-  center: [-70.6693, -33.4489],
-  zoom: 12,
+  center: [-72.024, -37.431],
+  zoom: 7,
 });
+
+let handleMapClick = e => {
+  //   console.log(e.point);
+  let features = map.queryRenderedFeatures(e.point);
+
+  if (features.length === 1) {
+    // console.log(features);
+    let schoolDiv = $("#schoolDiv");
+
+    schoolDiv.html(`<h1>${features[0].properties.idrbd}</h1>`);
+  }
+};
+
+map.on("click", handleMapClick);
 
 // d3.json("./data/viz/geoOutput.json").then(geojson => {
 //   console.log(geojson);
